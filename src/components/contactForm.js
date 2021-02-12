f1c31e710189cd2b2bf2285f56da2a7751c5f9cd
Ivo <ilegzdins@yahoo.com>
2021-02-12 18:18:01 +0000

    formik added

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -1,102 +1,128 @@
 import React from 'react';
-import { navigate } from 'gatsby-link';
-
-function encode(data) {
+import { Formik, Field } from 'formik';
+import validationSchema from './validationSchema';
+const encode = (data) => {
   return Object.keys(data)
     .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
     .join('&');
-}
+};
 
-export default class ContactForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { isValidated: false };
-  }
+const ContactForm = () => {
+  return (
+    <Formik
+      initialValues={{ name: '', email: '', message: '', success: false }}
+      validationSchema={validationSchema}
+      onSubmit={(values, { setSubmitting, resetForm, setFieldValue }) => {
+        fetch('/?no-cache=1', {
+          //eslint-disable-line
+          method: 'POST',
+          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+          body: encode({
+            'form-name': 'contact',
+            ...values,
+          }),
+        })
+          .then(() => {
+            setFieldValue('success', true);
+            setTimeout(() => resetForm(), 6000);
+            setSubmitting(false);
+          })
+          .catch((error) => {
+            console.log(error);
+            setFieldValue('success', false);
+            alert('Error: Please Try Again!'); //eslint-disable-line
+            setSubmitting(false);
+          });
+      }}
+      render={({
+        errors,
+        touched,
+        isSubmitting,
+        handleSubmit,
+        handleReset,
+        values,
+      }) => (
+        <section className=' flex justify-center md:w-1/2'>
+          <form
+            className='px-10 mx-auto w-full md:w-7/12'
+            name='contact'
+            onSubmit={handleSubmit}
+            onReset={handleReset}
+            data-netlify='true'
+            data-netlify-honeypot='bot-field'
+          >
+            <div>
+              <div className='mt-8'>
+                <Field
+                  className='rounded w-full'
+                  type='text'
+                  placeholder='Name'
+                  name='name'
+                  id='name'
+                />
+              </div>
+              {touched.name && errors.name && (
+                <small className='text-danger'>{errors.name}</small>
+              )}
+            </div>
 
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+            <div>
+              <div className='mt-6'>
+                <Field
+                  className='rounded w-full'
+                  type='email'
+                  placeholder='Email'
+                  name='email'
+                  id='email'
+                />
+              </div>
+              {touched.email && errors.email && (
+                <small className='text-danger'>{errors.email}</small>
+              )}
+            </div>
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    const form = e.target;
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({
-        'form-name': form.getAttribute('name'),
-        ...this.state,
-      }),
-    })
-      .then(() => navigate(form.getAttribute('action')))
-      .catch((error) => alert(error));
-  };
+            <div>
+              <div className='mt-6'>
+                <Field
+                  className='rounded w-full h-16'
+                  component='textarea'
+                  placeholder='Message'
+                  name='message'
+                  id='message'
+                  rows='4'
+                />
+              </div>
+              {touched.message && errors.message && (
+                <small className='text-danger'>{errors.message}</small>
+              )}
+            </div>
 
-  render() {
-    return (
-      <section className=' flex justify-center md:w-1/2'>
-        <form
-          className='px-10 mx-auto w-full md:w-7/12'
-          name='contact'
-          method='post'
-          action='/'
-          data-netlify='true'
-          data-netlify-honeypot='bot-field'
-          onSubmit={this.handleSubmit}
-        >
-          {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-          <input type='hidden' name='form-name' value='contact' />
-          <div hidden>
-            <label>
-              Don’t fill this out:{' '}
-              <input name='bot-field' onChange={this.handleChange} />
-            </label>
-          </div>
+            <div>
+              {values.success && (
+                <div>
+                  <div>
+                    <h4>
+                      Your message has been successfully sent, I will get back
+                      to you ASAP!
+                    </h4>
+                  </div>
+                </div>
+              )}
+              <div className='mt-5 mb-8'>
+                <button
+                  className='bg-pink-700 hover:bg-pink-800 text-white font-bold py-2 px-7 rounded-full'
+                  type='submit'
+                  disabled={isSubmitting}
+                >
+                  Send
+                </button>
+              </div>
+            </div>
+          </form>
+        </section>
+      )}
+    />
+  );
+};
 
-          <div className='mt-8'>
-            <input
-              className='rounded w-full'
-              type={'text'}
-              name={'name'}
-              placeholder='Name'
-              onChange={this.handleChange}
-              id={'name'}
-              required={true}
-            />
-          </div>
-
-          <div className='mt-6'>
-            <input
-              className='rounded w-full'
-              placeholder='Email'
-              type={'email'}
-              name={'email'}
-              onChange={this.handleChange}
-              id={'email'}
-              required={true}
-            />
-          </div>
-
-          <div className='mt-6'>
-            <textarea
-              className=' rounded w-full'
-              name={'message'}
-              placeholder='Message'
-              onChange={this.handleChange}
-              id={'message'}
-              required={true}
-            />
-          </div>
-          <div className='mt-5 mb-8'>
-            <button
-              className='bg-pink-700 hover:bg-pink-800 text-white font-bold py-2 px-7 rounded-full'
-              type='submit'
-            >
-              Send
-            </button>
-          </div>
-        </form>
-      </section>
-    );
-  }
-}
+export default ContactForm;
